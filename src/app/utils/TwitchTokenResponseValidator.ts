9449d081dd17af0b8a701217ab337b6457046566
Thomas <thomas.blakely@gmx.com>
2023-11-03 00:45:15 +1300

    Update TwitchTokenResponseValidator.ts

--- src/app/utils/TwitchTokenResponseValidator.ts
+++ src/app/utils/TwitchTokenResponseValidator.ts
@@ -4,19 +4,19 @@ import { InvalidTwitchResponseError } from '../models/error.model';
 export class TwitchTokenResponseValidator {
 
     public static async parseResponse(responseBody: string): Promise<TwitchTokenDetails> {
-        var tokenResponse = JSON.parse(JSON.stringify(responseBody));
+        const tokenResponse = JSON.parse(JSON.stringify(responseBody));
 
         try {
             console.info("Validating Twitch Response");
 
-            let tokenDetails = new TwitchTokenDetails(
+            const tokenDetails = new TwitchTokenDetails(
                 tokenResponse.access_token,
                 tokenResponse.refresh_token,
                 tokenResponse.expires_in,
                 tokenResponse.scope,
                 tokenResponse.token_type
             );
-            let completeConfigErrors = await validate(tokenDetails);
+            const completeConfigErrors = await validate(tokenDetails);
 
             if (completeConfigErrors.length > 0)
                 throw new InvalidTwitchResponseError(`The answer from twitch token endpoint is not valid, 
