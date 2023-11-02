17bb9aa3e019fe46fceb526eb4a752e067ec1c5a
Thomas <thomas.blakely@gmx.com>
2023-11-03 00:31:29 +1300

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,20 +2,8 @@ import { ChatBotConfig } from './src/app/config/config.model';
 import { ConfigValidator } from './src/app/config/config-validator';
 import { TwitchChatBot } from './src/app/chatBot/chatbot';
 
-
-ConfigValidator.readConfig(('./config.json'))
-.then((config: ChatBotConfig) =>  new TwitchChatBot(config).launch());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ConfigValidator
+  .readConfig(('./config.json'))
+  .then((
+    config: ChatBotConfig) =>  new TwitchChatBot(config).launch()
+  );
